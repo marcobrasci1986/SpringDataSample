b6fb9e4cf3c63e0426ca8e885377f3aecff24b68
MarcoBrasci1986 <marco_brasci@hotmail.com>
2016-03-21 11:25:07 +0100

    Add module 2

--- src/main/java/com/guitar/db/repository/LocationJpaRepository.java
+++ src/main/java/com/guitar/db/repository/LocationJpaRepository.java
@@ -2,9 +2,11 @@ package com.guitar.db.repository;
 
 import com.guitar.db.model.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by MaBa on 21/03/16.
  */
+@Repository
 public interface LocationJpaRepository extends JpaRepository<Location, Long> {
 }
--- src/main/java/com/guitar/db/repository/ModelTypeJpaRepository.java
+++ src/main/java/com/guitar/db/repository/ModelTypeJpaRepository.java
@@ -2,9 +2,11 @@ package com.guitar.db.repository;
 
 import com.guitar.db.model.ModelType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by MaBa on 21/03/16.
  */
+@Repository
 public interface ModelTypeJpaRepository extends JpaRepository<ModelType, Long> {
 }
